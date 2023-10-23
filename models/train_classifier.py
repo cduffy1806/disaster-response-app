0a0f21185e7a288c561a3af60239afe5ef046a15
Conor Duffy <coduffy@hubspot.com>
2023-10-23 09:37:09 +0100

    fix formatting

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,4 +1,23 @@
+"""
+This script loads data from a database, tokenizes text data, builds a classification model
+, trains the model,evaluates the model's performance, and saves the trained model 
+to a pickle file.
+
+Functions:
+    load_data: Load data from database.
+    tokenize: Tokenize text data.
+    save_model: Save the trained model to a pickle file.
+    build_model: Build the classification model.
+    main: Main function of the script.
+
+Classes:
+    None
+
+Author:
+   Conor Duffy 
+"""
 import sys
+import pickle
 import pandas as pd
 from sqlalchemy import create_engine
 import nltk
@@ -8,13 +27,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.multioutput import MultiOutputClassifier
-from sklearn.metrics import classification_report,accuracy_score
+from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
-import pickle
 
-nltk.download(['punkt', 'wordnet'])
 
+nltk.download(["punkt", "wordnet"])
 
 
 def load_data(database_filepath):
@@ -28,13 +46,13 @@ def load_data(database_filepath):
         tuple: A tuple containing the features (X) and the targets (Y).
     """
     # load data from database
-    engine = create_engine(f'sqlite:///{database_filepath}')
+    engine = create_engine(f"sqlite:///{database_filepath}")
     # read sql table into dataframe df
-    df = pd.read_sql_table('dr_messages_categorized', engine)
-    X = df.loc[:,'message'].values
-    Y = df.iloc[:, 4:]
-    category_names = Y.columns
-    return X,Y,category_names 
+    df = pd.read_sql_table("dr_messages_categorized", engine)
+    x = df.loc[:, "message"].values
+    y = df.iloc[:, 4:]
+    category_names = y.columns
+    return x, y, category_names
 
 
 def tokenize(text):
@@ -60,26 +78,27 @@ def tokenize(text):
 
 
 def build_model():
-
     """
     Build a Random Forest Classifier model for multi-output classification.
 
     Returns:
         object: A Random Forest Classifier model to be trained.
     """
-    pipeline = Pipeline([
-                ('vect', CountVectorizer(tokenizer=tokenize)),
-                ('tfidf', TfidfTransformer()),
-                ('clf', MultiOutputClassifier(RandomForestClassifier()))
-    ])
+    pipeline = Pipeline(
+        [
+            ("vect", CountVectorizer(tokenizer=tokenize)),
+            ("tfidf", TfidfTransformer()),
+            ("clf", MultiOutputClassifier(RandomForestClassifier())),
+        ]
+    )
 
     parameters = {
-    # 'vect__ngram_range': ((1, 1), (1, 2)),
-    # 'vect__max_df': (0.5, 0.75, 1.0),
-    # 'vect__max_features': (None, 5000, 10000),
-    'tfidf__use_idf': (True, False),
-    # 'clf__estimator__n_estimators': [50, 100, 200],  
-    # 'clf__estimator__min_samples_split': [2, 3, 4]
+        # 'vect__ngram_range': ((1, 1), (1, 2)),
+        # 'vect__max_df': (0.5, 0.75, 1.0),
+        # 'vect__max_features': (None, 5000, 10000),
+        "tfidf__use_idf": (True, False),
+        # 'clf__estimator__n_estimators': [50, 100, 200],
+        # 'clf__estimator__min_samples_split': [2, 3, 4]
     }
 
     # Create GridSearch object and fit it to the data
@@ -88,7 +107,7 @@ def build_model():
     return cv
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
+def evaluate_model(model, x_test, y_test, category_names):
     """
     Evaluate the performance of the trained model on the test data.
 
@@ -99,21 +118,26 @@ def evaluate_model(model, X_test, Y_test, category_names):
         category_names (list): The list of category names.
     """
     # Predict on the test data
-    Y_pred = model.predict(X_test)
+    y_pred = model.predict(x_test)
 
     # Calculate the accuracy for each of them.
-    for i, column in enumerate(Y_test.columns):
-        accuracy = accuracy_score(Y_test.iloc[:, i].values, Y_pred[:, i])
-        print(f'Category: {column}')
-        print(f'Accuracy: {accuracy:.2f} \n')
+    for i, column in enumerate(y_test.columns):
+        accuracy = accuracy_score(y_test.iloc[:, i].values, y_pred[:, i])
+        print(f"Category: {column}")
+        print(f"Accuracy: {accuracy:.2f} \n")
 
     # Now we create a classification report for each of the 36 categories
     for i, category_name in enumerate(category_names):
-        print("Category:", category_name, "\n", classification_report(Y_test.iloc[:, i], Y_pred[:, i]))
+        print(
+            "Category:",
+            category_name,
+            "\n",
+            classification_report(y_test.iloc[:, i], y_pred[:, i]),
+        )
 
     # Overall accuracy
-    accuracy = (Y_pred == Y_test).mean().mean()
-    print(f'Overall Accuracy: {accuracy*100:.2f}% \n')  
+    accuracy = (y_pred == y_test).mean().mean()
+    print(f"Overall Accuracy: {accuracy*100:.2f}% \n")
 
 
 def save_model(model, model_filepath):
@@ -124,41 +148,42 @@ def save_model(model, model_filepath):
         model (object): The trained model.
         model_filepath (str): The filepath to save the model.
     """
-    with open(model_filepath, 'wb') as file:
+    with open(model_filepath, "wb") as file:
         pickle.dump(model, file)
 
 
 def main():
-    
     """
     Main function to execute the training and evaluation of the model.
     """
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
-        print(f'Loading data...\n    DATABASE: {database_filepath}')
-        X, Y, category_names = load_data(database_filepath)
-        x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
-        
-        print('Building model...')
+        print(f"Loading data...\n    DATABASE: {database_filepath}")
+        x, y, category_names = load_data(database_filepath)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+        print("Building model...")
         model = build_model()
-        
-        print('Training model...')
+
+        print("Training model...")
         model.fit(x_train, y_train)
-        
-        print('Evaluating model...')
+
+        print("Evaluating model...")
         evaluate_model(model, x_test, y_test, category_names)
 
-        print(f'Saving model...\n    MODEL: {model_filepath}')
+        print(f"Saving model...\n    MODEL: {model_filepath}")
         save_model(model, model_filepath)
 
-        print('Trained model saved!')
+        print("Trained model saved!")
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+        print(
+            "Please provide the filepath of the disaster messages database "
+            "as the first argument and the filepath of the pickle file to "
+            "save the model to as the second argument. \n\nExample: python "
+            "train_classifier.py ../data/DisasterResponse.db classifier.pkl"
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
